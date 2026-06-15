8c1ac6c353b117f0b78e02a59d0922221b259897
Yaroslav19940817 <pylypets777@gmail.com>
2026-06-15 10:11:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 LIMIT_MINUTES = 240  # 4 години
-
 already_sent = False
 
 
@@ -18,38 +17,35 @@ def send_message(text):
             "text": text
         }
     )
-
     print("TELEGRAM STATUS:", response.status_code)
 
 
 while True:
     try:
         response = requests.get(
-    "https://back.echerha.gov.ua/api/v4/workload/1",
-    headers={
-        "Accept": "application/json",
-        "User-Agent": "Mozilla/5.0",
-        "Referer": "https://echerha.gov.ua/"
-    },
-    timeout=30
-)
+            "https://back.echerha.gov.ua/api/v4/workload/1",
+            headers={
+                "Accept": "application/json",
+                "User-Agent": "Mozilla/5.0",
+                "Referer": "https://echerha.gov.ua/"
+            },
+            timeout=30
+        )
 
-print("STATUS:", response.status_code)
-print("TEXT:", response.text[:500])
-
-response.raise_for_status()
+        print("STATUS:", response.status_code)
+        print("TEXT:", response.text[:300])
 
         data = response.json()
 
         chop = next(
-            x for x in data["data"]
-            if x["id"] == 17
+            item for item in data["data"]
+            if item["id"] == 17
         )
 
         wait_time = chop["wait_time"]
         vehicles = chop["vehicle_in_active_queues_counts"]
 
-        print("WAIT TIME:", wait_time)
+        print("WAIT:", wait_time)
         print("VEHICLES:", vehicles)
 
         if wait_time >= LIMIT_MINUTES and not already_sent:
@@ -63,7 +59,6 @@ response.raise_for_status()
             )
 
             already_sent = True
-            print("Notification sent")
 
         elif wait_time < LIMIT_MINUTES:
             already_sent = False
@@ -71,5 +66,4 @@ response.raise_for_status()
     except Exception as e:
         print("ERROR:", str(e))
 
-    # перевірка кожні 15 хвилин
     time.sleep(900)
